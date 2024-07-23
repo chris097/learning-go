a3e1757c85b46100e70d53f11e16fab45f5f53d6
chris097 <chrisfidel.international@gmail.com>
2024-07-23 15:25:48 +0800

    integer in go

--- main.go
+++ main.go
@@ -2,6 +2,28 @@ package main
 
 import "fmt"
 
+/**
+params
+-- How to declare variables
+**/
+
 func main() {
-	fmt.Println("Hello team")
+	// Var declaration
+	var name string = "Christian Chiemela"
+	name = "Christian Chiemela"
+	var names string
+	// Don't declare shorthand outside of the func it won't work
+	named := "Christian Chiemela"
+	fmt.Println(name, names, named)
+
+	// integer
+	var numberOne int = 2
+	numberTwo := 4
+	fmt.Println(numberOne, numberTwo)
+
+	// bit & memory
+	var numberThree int8 = 127
+	// unit not negative number
+	var numberFour uint = 25
+	fmt.Println(numberThree, numberFour)
 }
