c177be5783d26175fb29048377f8aa53072130ff
chris097 <chrisfidel.international@gmail.com>
2024-07-26 19:03:59 +0800

    bug fix

--- main.go
+++ main.go
@@ -1,43 +1,29 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
+	"github.com/gorilla/mux"
 	"log"
+	"math/rand"
 	"net/http"
+	"strconv"
 )
 
-func formHandler(res http.ResponseWriter, req *http.Request) {
-	if err := req.ParseForm(); err != nil {
-		fmt.Fprintf(res, "ParseForm() err: %v", err)
-		return
-	}
-	fmt.Fprintf(res, "POST request successfully")
-	name := req.FormValue("name")
-	address := req.FormValue("address")
-	fmt.Fprintf(res, "Name = %v\n", name)
-	fmt.Fprintf(res, "Address = %v\n", address)
+type Movie struct {
+	ID       string    `json:"id"`
+	Isbn     string    `json:"isbn"`
+	Title    string    `json:"title"`
+	Director *Director `json:"director"`
 }
 
-func helloHandler(res http.ResponseWriter, req *http.Request) {
-	if req.URL.Path != "/hello" {
-		http.Error(res, "404 not found", http.StatusNotFound)
-		return
-	}
-	if req.Method != "GET" {
-		http.Error(res, "method is not supported", http.StatusNotFound)
-		return
-	}
-	fmt.Fprintf(res, "Hello!")
+type Director struct {
+	Firstname string `json:"firstname"`
+	Lastname  string `json:"lastname"`
 }
+
+var movies []Movie
 
 func main() {
-	fileServer := http.FileServer(http.Dir("./static"))
-	http.Handle("/", fileServer)
-	http.HandleFunc("/form", formHandler)
-	http.HandleFunc("/hello", helloHandler)
-
-	fmt.Printf("Starting server at port 8080\n")
-	if err := http.ListenAndServe(":8080", nil); err != nil {
-		log.Fatal(err)
-	}
+	r := mux.NewRouter()
 }
