18ae4c687484bd0b64700013dc9471e769b8d7db
chris097 <chrisfidel.international@gmail.com>
2024-07-26 09:21:09 +0800

    web server with go

--- main.go
+++ main.go
@@ -1,122 +1,43 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"os"
-	"strconv"
-	"strings"
+	"log"
+	"net/http"
 )
 
-/**
-params
--- How to declare variables
-**/
-
-func getInput(prompt string, r *bufio.Reader) (string, error) {
-	fmt.Print(prompt)
-	input, err := r.ReadString('\n')
-
-	return strings.TrimSpace(input), err
-}
-
-func createBill() bill {
-	reader := bufio.NewReader(os.Stdin)
-
-	// fmt.Print("Create a new bill: ")
-	// name, _ := reader.ReadString('\n')
-	// name = strings.TrimSpace(name)
-
-	name, _ := getInput("Create a new bill: ", reader)
-
-	b := newBill(name)
-	fmt.Println("Created the bill - ", b.name)
-
-	return b
-}
-
-func promptOption(b bill) {
-	reader := bufio.NewReader(os.Stdin)
-
-	opt, _ := getInput("Choose option (a - add Item, s - save bill, t - add tip): ", reader)
-
-	switch opt {
-	case "a":
-		name, _ := getInput("Item name ", reader)
-		price, _ := getInput("Item Price ", reader)
-
-		p, err := strconv.ParseFloat(price, 64)
-		if err != nil {
-			fmt.Println("The price must be a number")
-			promptOption(b)
-		}
-		b.addItem(name, p)
-
-		fmt.Println("Item added - ", name, price)
-		promptOption(b)
-	case "t":
-		tip, _ := getInput("Enter tip amount ", reader)
-
-		t, err := strconv.ParseFloat(tip, 64)
-		if err != nil {
-			fmt.Println("The tip must be a number")
-			promptOption(b)
-		}
-		b.updateTip(t)
-
-		fmt.Println("Tip added - ", tip)
-		promptOption(b)
-	case "s":
-		b.save()
-		fmt.Println("You saved the file - ", b.name)
-	default:
-		fmt.Println("Your answer not a valid option...")
-		promptOption(b)
+func formHandler(res http.ResponseWriter, req *http.Request) {
+	if err := req.ParseForm(); err != nil {
+		fmt.Fprintf(res, "ParseForm() err: %v", err)
+		return
 	}
+	fmt.Fprintf(res, "POST request successfully")
+	name := req.FormValue("name")
+	address := req.FormValue("address")
+	fmt.Fprintf(res, "Name = %v\n", name)
+	fmt.Fprintf(res, "Address = %v\n", address)
+}
+
+func helloHandler(res http.ResponseWriter, req *http.Request) {
+	if req.URL.Path != "/hello" {
+		http.Error(res, "404 not found", http.StatusNotFound)
+		return
+	}
+	if req.Method != "GET" {
+		http.Error(res, "method is not supported", http.StatusNotFound)
+		return
+	}
+	fmt.Fprintf(res, "Hello!")
 }
 
 func main() {
-	// Var declaration
-	// var name string = "Christian Chiemela"
-	// name = "Christian Chiemela"
-	// var names string
-	// // Don't declare shorthand outside of the func it won't work
-	// named := "Christian Chiemela"
-	// fmt.Println(name, names, named)
+	fileServer := http.FileServer(http.Dir("./static"))
+	http.Handle("/", fileServer)
+	http.HandleFunc("/form", formHandler)
+	http.HandleFunc("/hello", helloHandler)
 
-	// // integer
-	// var numberOne int = 2
-	// numberTwo := 4
-	// fmt.Println(numberOne, numberTwo)
-
-	// // bit & memory
-	// var numberThree int8 = 127
-	// // unit not negative number
-	// var numberFour uint = 25
-	// fmt.Println(numberThree, numberFour)
-
-	// // Arrays
-
-	// var nameone [3]string = [3]string{"hello", "body", "new"}
-
-	// var nametwo = []int{1, 34, 5}
-	// namethree := append(nametwo, 50)
-	// fmt.Println(nameone, nametwo, namethree)
-
-	// bill := newBill("christian chiemela")
-
-	// bill.addItem("cake", 5.50)
-	// bill.addItem("tea", 3.50)
-	// bill.addItem("bread", 2.50)
-
-	// bill.updateTip(10)
-
-	// fmt.Println(bill.format())
-
-	myBill := createBill()
-
-	promptOption(myBill)
-
-	// fmt.Println(myBill)
-
+	fmt.Printf("Starting server at port 8080\n")
+	if err := http.ListenAndServe(":8080", nil); err != nil {
+		log.Fatal(err)
+	}
 }
